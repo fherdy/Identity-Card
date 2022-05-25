e5762c4eb99ee2f370a527c623f47543089aac1b
ferdinand <paa1000@yahoo.com>
2022-05-25 00:40:16 -0400

    class to handle sending emails

---
+++ notificationManager.py
@@ -0,0 +1,31 @@
+import os
+from twilio.rest import Client
+import smtplib
+
+
+SENDER_EMAIL = "ferdie.tettey@yahoo.com"
+PASSWORD = "fhecbuvngrutwtlz"
+
+class NotificationManager:
+    #This class is responsible for sending notifications with the deal flight details.
+    def __init__(self):
+        self.message = ""
+
+    def send_email(self, name, email, message):
+        self.name = name
+        self.emal = email
+        self.message = message
+
+        with smtplib.SMTP("smtp.mail.yahoo.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=SENDER_EMAIL, password=PASSWORD)
+
+            try:
+                connection.sendmail(from_addr=SENDER_EMAIL,
+                                to_addrs="ferdinandowusu13@gmail.com",
+                                msg=f"Subject:You just got contacted by {self.name}! \n\nName: {self.name} \nEmail: {self.emal}\nMessage: {self.message}")
+                print("Successfully Sent!")
+            except (smtplib.SMTPResponseException, smtplib.SMTPRecipientsRefused, smtplib.SMTPAuthenticationError):
+                print(f"{self.receipient_email} is an invalid email")
+
+
